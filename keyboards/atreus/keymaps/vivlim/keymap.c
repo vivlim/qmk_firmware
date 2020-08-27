2555ccf4bdc552ce2c0b15489cb9f033ed3e4087
Vivian Lim <vivlim@pm.me>
2020-08-27 02:53:00 -0700

    updates, especially to the symbol layer.

--- keyboards/atreus/keymaps/vivlim/keymap.c
+++ keyboards/atreus/keymaps/vivlim/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     MT(MOD_LSFT, KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    MT(MOD_LCTL, KC_ESC), KC_TAB, KC_LGUI,  KC_LALT, MT(MOD_LCTL, KC_ENT),  MO(_RS), LT(_SYM, KC_BSPC), KC_SPC,  KC_LSFT, KC_MINS, KC_QUOT, KC_ENT
+    MT(MOD_LCTL, KC_ESC), KC_LGUI,  KC_LALT, KC_SPC, MT(MOD_LCTL, KC_ENT),  MO(_RS), LT(_SYM, KC_BSPC), KC_SPC,  KC_LSFT, KC_MINS, KC_QUOT, KC_ENT
   ),
 
   /*
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * lower  insert super shift bksp ctrl || alt space   fn    .     0    =
    */
   [_RS] = LAYOUT( /* [> RAISE <] */
-    KC_EXLM, KC_HOME, KC_UP,   KC_END, KC_RCBR,                     KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
-    KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
+    KC_TAB, KC_HOME, KC_UP,   KC_END, KC_TILDE,                     KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
+    KC_ESC, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_PGDN,                   KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-    MO(_LW), KC_INS,  KC_LGUI, KC_LALT, MT(MOD_LCTL, KC_ENT), KC_TRNS, KC_DEL, KC_SPC,  KC_0, KC_DOT, KC_0, KC_EQL  ),
+    MO(_LW), KC_LGUI, KC_LALT, KC_SPC, MT(MOD_LCTL, KC_ENT), KC_TRNS, KC_DEL, KC_SPC,  KC_0, KC_DOT, KC_0, KC_EQL  ),
   /*
    * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
    *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
@@ -43,11 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DEL,  KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
     LGUI(LCTL(LALT(KC_ENT))),   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_QW), KC_PSCR, KC_SLCK, KC_PAUS ),
+    KC_NO,   KC_LGUI, KC_LALT, KC_SPC, KC_LCTL, KC_NO, KC_NO,      KC_SPC, KC_LSFT, TO(_QW), KC_PSCR, KC_SLCK),
+
+    //MT(MOD_LCTL, KC_ESC), KC_LGUI,  KC_LALT, KC_SPC, MT(MOD_LCTL, KC_ENT),  MO(_RS), LT(_SYM, KC_BSPC), KC_SPC,  KC_LSFT, KC_MINS, KC_QUOT, KC_ENT
 
   [_SYM] = LAYOUT( /* [> SYMBOLS <] */
-    KC_EXLM,  LSFT(KC_COMMA), LSFT(KC_DOT),   KC_LBRC,  KC_RBRC,                   KC_CIRCUMFLEX,   KC_DOLLAR,   KC_AMPERSAND,   KC_NO,   KC_NO,
-    KC_AT,  KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN,                   KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_HASH,   KC_NO, KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+    KC_EXLM,  LSFT(KC_COMMA), LSFT(KC_DOT),   KC_LBRC,  KC_RBRC,                   KC_CIRCUMFLEX,   KC_DOLLAR,   KC_AMPERSAND,   KC_PERC,   KC_PIPE,
+    KC_AT,  KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN,                   KC_TILDE, KC_QUOT,   KC_MINUS,   KC_NO,   KC_COLON,
+    KC_HASH,   KC_NO, KC_NO,   KC_NO,   KC_NO,                     KC_GRAVE,   KC_DQUO,   KC_UNDS,   KC_NO,   KC_BSLASH,
     KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,  KC_NO, KC_NO, KC_NO, RESET )
 };
